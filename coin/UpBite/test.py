140d517708cfbc7e29519c0b6769d38d18f544b8
redbeers <71654161+redbeers@users.noreply.github.com>
2022-04-08 22:25:28 +0900

    .

--- coin/UpBite/test.py
+++ coin/UpBite/test.py
@@ -6,30 +6,19 @@ from urllib.parse import urlencode
 
 import requests
 
-access_key = os.environ['Nxt7RzoXxXkjD1ZZrkSrlx5Bs9sGYZxC3kx2bERI']
-secret_key = os.environ['qpdOHHx20MoRof6Rs0MoxTaNDzxcRUPACkMKdwMD']
-server_url = os.environ['https://api.upbit.com/v1/order']
+access_key = 'Nxt7RzoXxXkjD1ZZrkSrlx5Bs9sGYZxC3kx2bERI'
+secret_key = 'VCLoAhrxbvyrukYChbxfxD6O1ESegeckIgbqeiQf'
+server_url = 'https://api.upbit.com/v1/accounts'
 
-query = {
-    'uuid': '9ca023a5-851b-4fec-9f0a-48cd83c2eaae',
-}
-query_string = urlencode(query).encode()
+# payload = {
+#     'access_key': 'Nxt7RzoXxXkjD1ZZrkSrlx5Bs9sGYZxC3kx2bERI',
+#     'nonce': str(uuid.uuid4()),
+# }
 
-m = hashlib.sha512()
-m.update(query_string)
-query_hash = m.hexdigest()
-
-payload = {
-    'access_key': access_key,
-    'nonce': str(uuid.uuid4()),
-    'query_hash': query_hash,
-    'query_hash_alg': 'SHA512',
-}
-
-jwt_token = jwt.encode(payload, secret_key)
+jwt_token = jwt.encode('Nxt7RzoXxXkjD1ZZrkSrlx5Bs9sGYZxC3kx2bERI', 'VCLoAhrxbvyrukYChbxfxD6O1ESegeckIgbqeiQf')
 authorize_token = 'Bearer {}'.format(jwt_token)
 headers = {"Authorization": authorize_token}
 
-res = requests.get(server_url + "/v1/order", params=query, headers=headers)
+res = requests.get("https://api.upbit.com/v1/accounts", headers=headers)
 
 print(res.json())
